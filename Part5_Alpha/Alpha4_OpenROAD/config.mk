acbcd06a37912b2a93a63981289022bd21211fad
Matthew Jarecky <114567786+mjarecky@users.noreply.github.com>
2025-12-13 23:21:36 -0800

    Update config.mk to work with current orfs

--- Part5_Alpha/Alpha4_OpenROAD/config.mk
+++ Part5_Alpha/Alpha4_OpenROAD/config.mk
@@ -4,8 +4,8 @@ export DESIGN_NAME      = core
 export ADDITIONAL_LEFS += $(PLATFORM_DIR)/lef/RM_IHPSG13_1P_2048x64_c2_bm_bist.lef \
 						  additional_tech_sram_files/RM_IHPSG13_1P_512x32_c2_bm_bist.lef
 
-export ADDITIONAL_LIBS += $(PLATFORM_DIR)/lib/RM_IHPSG13_1P_2048x64_c2_bm_bist_typ_1p20V_25C.lib \
-					      additional_tech_sram_files/RM_IHPSG13_1P_512x32_c2_bm_bist_typ_1p20V_25C.lib
+export ADDITIONAL_TYP_LIBS += $(PLATFORM_DIR)/lib/RM_IHPSG13_1P_2048x64_c2_bm_bist_typ_1p20V_25C.lib \
+					          additional_tech_sram_files/RM_IHPSG13_1P_512x32_c2_bm_bist_typ_1p20V_25C.lib
 
 export ADDITIONAL_GDS  += $(PLATFORM_DIR)/gds/RM_IHPSG13_1P_2048x64_c2_bm_bist.gds \
 						  additional_tech_sram_files/RM_IHPSG13_1P_512x32_c2_bm_bist.gds
